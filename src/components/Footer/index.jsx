c1e7d6887005543d779f37dc20b46c73d0118288
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-06-23 12:04:19 -0300

    adicionado animações e link para github

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Footer({ onClick, primary }) {
     return (
         <footer className={styles.footer}>
+            <a target="_blank" href="https://github.com/gustavohernandes11/memory-game">
+                <FontAwesomeIcon icon={faGithub} />
+                Ver no github
+            </a>
+
+
             <button primary={primary} onClick={onClick}>Reiniciar</button>
+
         </footer>
     )
 }
